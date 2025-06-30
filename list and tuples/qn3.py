bdd65b6ae1c77db1a5c743ef4aab59d11fd34aef
AyushNaik7 <ayushnaik9898@gmail.com>
2025-06-30 08:26:42 +0530

    qn3and4

---
+++ list and tuples/qn3.py
@@ -0,0 +1,11 @@
+numbers = []
+num1 = int(input("enter the num1 :"))
+numbers.append(num1)
+num2 = int(input("enter the num1 :"))
+numbers.append(num2)
+num3 = int(input("enter the num1 :"))
+numbers.append(num3)
+num4 = int(input("enter the num1 :"))
+numbers.append(num4)
+
+print(sum(numbers))
---
+++ list and tuples/qn4.py
@@ -0,0 +1,3 @@
+tuple = (7,0,8,0,0,9)
+c = tuple.count(0)  
+print(c)

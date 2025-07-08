ed1429921b47e480ff75759cc7b7e0dcc49565b6
AyushNaik7 <ayushnaik9898@gmail.com>
2025-07-08 22:15:16 +0530

    if_else

---
+++ conditionals_statements.py/if_else.py
@@ -0,0 +1,10 @@
+a = int(input ("enter your age : "))
+
+if(a>18):
+    print("your age is above the age of 18 :")
+    print("you can vote")
+
+else:
+    print("you are below the age of 18 you cant vote")
+
+print("end of the program")
